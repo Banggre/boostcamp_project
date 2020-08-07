bc3088bbfb97c075063ede588e17a5736ed87dc9
두주 <dlgkswn885@korea.ac.kr>
2020-08-07 14:59:20 +0900

    1

---
+++ src/components/views/VideoUploadPage/VideoUploadPage.js
@@ -0,0 +1,211 @@
+import React, { useState, useEffect } from "react";
+import { Typography, Button, Form, message, Input, Icon } from "antd";
+import Dropzone from "react-dropzone";
+import axios from "axios";
+import { useSelector } from "react-redux";
+
+const { Title } = Typography;
+const { TextArea } = Input;
+
+// select를 사용할때 option으로 노가다 하는게 아닌 이렇게 Private를 정의한 후에 map method를 이용하여 넣는다
+const Private = [
+  { value: 0, label: "Private" },
+  { value: 1, label: "Public" },
+];
+
+/* 
+option의 key를 index로 할당해줘야 오류가 안생김
+  */
+
+const Catogory = [
+  { value: 0, label: "Film & Animation" },
+  { value: 0, label: "Autos & Vehicles" },
+  { value: 0, label: "Music" },
+  { value: 0, label: "Pets & Animals" },
+  { value: 0, label: "Sports" },
+];
+
+//  onchange함수를 구현해야 상태변화를 출력하는 것이 가능하다
+function UploadVideoPage(props) {
+  //  리덕스를 통해서 state에 가서 유저를 가지고 오는 것임
+  const user = useSelector((state) => state.user);
+
+  const [title, setTitle] = useState("");
+  const [Description, setDescription] = useState("");
+  const [privacy, setPrivacy] = useState(0);
+  const [Categories, setCategories] = useState("Film & Animation");
+  const [FilePath, setFilePath] = useState("");
+  const [Duration, setDuration] = useState("");
+  const [Thumbnail, setThumbnail] = useState("");
+
+  const handleChangeTitle = (event) => {
+    setTitle(event.currentTarget.value);
+  };
+
+  const handleChangeDecsription = (event) => {
+    console.log(event.currentTarget.value);
+
+    setDescription(event.currentTarget.value);
+  };
+
+  const handleChangeOne = (event) => {
+    setPrivacy(event.currentTarget.value);
+  };
+
+  const handleChangeTwo = (event) => {
+    setCategories(event.currentTarget.value);
+  };
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    // 원래 클릭을하면 되는걸 방지된것
+
+    if (user.userData && !user.userData.isAuth) {
+      return alert("Please Log in First");
+    }
+
+    if (
+      title === "" ||
+      Description === "" ||
+      Categories === "" ||
+      FilePath === "" ||
+      Duration === "" ||
+      Thumbnail === ""
+    ) {
+      return alert("Please first fill all the fields");
+    }
+    // redux를 통해서 유저 변수를 가지고 오는것임
+    const variables = {
+      writer: user.userData._id,
+      title: title,
+      description: Description,
+      privacy: privacy,
+      filePath: FilePath,
+      category: Categories,
+      duration: Duration,
+      thumbnail: Thumbnail,
+    };
+    // post보내고 variable을 정의
+    axios.post("/api/video/uploadVideo", variables).then((response) => {
+      if (response.data.success) {
+        message.success("성공적으로 업로드 했음");
+        // alert("video Uploaded Successfully");
+        // 3초 이후에 일어났으면 좋겠다
+        setTimeout(() => {
+          props.history.push("/");
+        }, 3000);
+      } else {
+        alert("Failed to upload video");
+      }
+    });
+  };
+
+  const onDrop = (files) => {
+    let formData = new FormData();
+    const config = {
+      header: { "content-type": "multipart/form-data" },
+    };
+    console.log(files);
+    formData.append("file", files[0]);
+
+    axios.post("/api/video/uploadfiles", formData, config).then((response) => {
+      if (response.data.success) {
+        let variable = {
+          filePath: response.data.filePath,
+          fileName: response.data.fileName,
+        };
+        setFilePath(response.data.filePath);
+
+        //gerenate thumbnail with this filepath !
+
+        axios.post("/api/video/thumbnail", variable).then((response) => {
+          if (response.data.success) {
+            setDuration(response.data.fileDuration);
+            setThumbnail(response.data.thumbsFilePath);
+          } else {
+            alert("Failed to make the thumbnails");
+          }
+        });
+      } else {
+        alert("failed to save the video in server");
+      }
+    });
+  };
+
+  return (
+    <div style={{ maxWidth: "700px", margin: "2rem auto" }}>
+      <div style={{ textAlign: "center", marginBottom: "2rem" }}>
+        <Title level={2}> Upload Video</Title>
+      </div>
+
+      <Form onSubmit={onSubmit}>
+        <div style={{ display: "flex", justifyContent: "space-between" }}>
+          <Dropzone onDrop={onDrop} multiple={false} maxSize={800000000}>
+            {({ getRootProps, getInputProps }) => (
+              <div
+                style={{
+                  width: "300px",
+                  height: "240px",
+                  border: "1px solid lightgray",
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "center",
+                }}
+                {...getRootProps()}
+              >
+                <input {...getInputProps()} />
+                <Icon type="plus" style={{ fontSize: "3rem" }} />
+              </div>
+            )}
+          </Dropzone>
+          {/*  thumbnail이 있을때만 올려라 */}
+          {Thumbnail !== "" && (
+            <div>
+              {/* 5000포트로 노드를 돌리고 있어서 그럼 */}
+              <img src={`http://localhost:5000/${Thumbnail}`} alt="haha" />
+            </div>
+          )}
+        </div>
+
+        <br />
+        <br />
+        <label>Title</label>
+        <Input onChange={handleChangeTitle} value={title} />
+        <br />
+        <br />
+        <label>Description</label>
+        <TextArea onChange={handleChangeDecsription} value={Description} />
+        <br />
+        <br />
+
+        <select onChange={handleChangeOne}>
+          {Private.map((item, index) => (
+            <option key={index} value={item.value}>
+              {item.label}
+            </option>
+          ))}
+        </select>
+        <br />
+        <br />
+
+        <select onChange={handleChangeTwo}>
+          {Catogory.map((item, index) => (
+            <option key={index} value={item.label}>
+              {item.label}
+            </option>
+          ))}
+        </select>
+        <br />
+        <br />
+
+        <Button type="primary" size="large" onClick={onSubmit}>
+          Submit
+        </Button>
+      </Form>
+    </div>
+  );
+}
+
+export default UploadVideoPage;
+
+// rfce를 쳐서 한번에 만들기
